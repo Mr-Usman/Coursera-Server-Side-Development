c82ebf85651cb670570411d38435eb99f3f7fcc6
usmanrehmat556 <usmanrehmat15@outlook.com>
2019-05-17 20:38:55 +0500

    node HTTP simple server

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const hostname = "localhost";
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  console.log(req.headers);
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>usman</h1></body></html>");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server is running at http://${hostname}:${port}`);
+});
