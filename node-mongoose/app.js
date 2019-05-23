69f3e6e0d46c84fd1fb1763225f4b86ac19fc422
usmanrehmat556 <usmanrehmat15@outlook.com>
2019-05-24 01:12:23 +0500

    Mongoose part 2

--- node-mongoose/app.js
+++ node-mongoose/app.js
@@ -12,13 +12,10 @@ const connect = mongoose.connect(url, {
 connect.then(db => {
   console.log("Connected correctly to server");
 
-  var newDish = Dishes({
+  Dishes.create({
     name: "pizza",
     description: "broadway"
-  });
-
-  newDish
-    .save()
+  })
     .then(dish => {
       console.log(dish);
       return Dishes.find({}).exec();
